d4390797eac1da654dce7db9c3ab4d532c4d07ba
swapnasunil57-create <swapnasunil57@gmail.com>
2026-01-07 10:18:26 +0530

    Create sum_avg_three.py

---
+++ Sequential problems/sum_avg_three.py
@@ -0,0 +1,16 @@
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+s = a + b + c
+avg = s / 3
+print("Sum =", s)
+print("Average =", avg)
+
+"""
+output:
+Enter a: 1
+Enter b: 2
+Enter c: 3
+Sum = 6
+Average = 2.0
+"""
