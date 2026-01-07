648317b3bf0bf7e09c7269c015aaa276c6c0d43a
swapnasunil57-create <swapnasunil57@gmail.com>
2026-01-07 10:15:33 +0530

    Create operation.py

---
+++ Sequential problems/operation.py
@@ -0,0 +1,19 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum =", a + b)
+print("Difference =", a - b)
+print("Product =", a * b)
+print("Quotient =", a / b)
+print("Remainder =", a % b)
+
+
+"""
+output:
+Enter first number: 12
+Enter second number: 13
+Sum = 25
+Difference = -1
+Product = 156
+Quotient = 0.9230769230769231
+Remainder = 12
+"""
