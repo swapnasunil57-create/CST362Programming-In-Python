897750517bcb85c919d27fece38353eb006419ea
swapnasunil57-create <swapnasunil57@gmail.com>
2026-01-07 10:09:37 +0530

    Create add_twonumbers.py

---
+++ Sequential problems/add_twonumbers.py
@@ -0,0 +1,10 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum =", a + b)
+
+"""
+output:
+Enter first number: 12
+Enter second number: 13
+Sum = 25
+"""
