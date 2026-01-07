b2ca4fad57c362c956c859eb8feaea21c6875153
swapnasunil57-create <swapnasunil57@gmail.com>
2026-01-07 10:44:58 +0530

    Create simple_interest.py

---
+++ Sequential problems/simple_interest.py
@@ -0,0 +1,13 @@
+p = float(input("Enter principal: "))
+r = float(input("Enter rate: "))
+t = float(input("Enter time: "))
+si = (p * r * t) / 100
+print("Simple Interest =", si)
+
+"""
+output:
+Enter principal: 200
+Enter rate: 2
+Enter time: 1
+Simple Interest = 4.0
+"""
