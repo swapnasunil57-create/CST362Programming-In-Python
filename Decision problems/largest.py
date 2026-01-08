a6f48ffaa838aed06cfdb25159fe38538b717725
swapnasunil57-create <swapnasunil57@gmail.com>
2026-01-08 18:46:23 +0530

    Create largest.py

---
+++ Decision problems/largest.py
@@ -0,0 +1,14 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print(a, "is largest")
+else:
+    print(b, "is largest")
+
+"""
+output
+Enter first number: 12
+Enter second number: 13
+13 is largest
+"""
