367045dcadb636ee9532b74ac00816565f2d17b5
swapnasunil57-create <swapnasunil57@gmail.com>
2026-01-08 18:30:44 +0530

    Create positive.py

---
+++ Decision problems/positive.py
@@ -0,0 +1,14 @@
+n = int(input("Enter a number: "))
+
+if n > 0:
+    print("Positive")
+elif n < 0:
+    print("Negative")
+else:
+    print("Zero")
+
+"""
+output
+Enter a number: 3
+Positive
+"""
