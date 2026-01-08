7e649e09b294e2159a98a3ac735039a6f057d8a3
swapnasunil57-create <swapnasunil57@gmail.com>
2026-01-08 18:42:23 +0530

    Update divisible.py

---
+++ Decision problems/divisible.py
@@ -0,0 +1,13 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a % b == 0:
+    print("Completely divisible")
+else:
+    print("Not completely divisible")
+"""
+output
+Enter first number: 10
+Enter second number: 2
+Completely divisible
+"""
