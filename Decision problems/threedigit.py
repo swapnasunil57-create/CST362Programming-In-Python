2a0e213a0adccdbc619399130e74620339bea52a
swapnasunil57-create <swapnasunil57@gmail.com>
2026-01-08 21:42:03 +0530

    Create threedigit.py

---
+++ Decision problems/threedigit.py
@@ -0,0 +1,13 @@
+n = int(input("Enter a number: "))
+
+if n >= 100 and n <= 999:
+    print("3-digit number")
+else:
+    print("Not a 3-digit number")
+
+
+"""
+output
+Enter a number: 234
+3-digit number
+"""
